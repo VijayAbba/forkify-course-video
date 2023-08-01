886cb9e6e1920575db2f0437107dbb8487f63367
VijayAbba <vijaykrishna.ed@gmail.com>
2023-08-01 14:31:04 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,20 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = '81984ef4-d45e-498b-a691-58320b82566e';
+
+export const MODEL_CLOSE_SEC = 2.5;
+/*
+confi
+helper
+
+
+var in const a re
+
+ex d some ok
+
+
+*/
